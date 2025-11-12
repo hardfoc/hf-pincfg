445cf3505feddcda7545f4c6de44ef64a6e57a94
Nebiyu Tadesse <nebiyutadesse@conmed.com>
2025-11-12 13:16:58 -0700

    Add configuration files for clang-format, clang-tidy, and yamllint; update error message formatting in hf_functional_pin_config.hpp

--- src/hf_functional_pin_config.hpp
+++ src/hf_functional_pin_config.hpp
@@ -5,7 +5,8 @@
 #if defined(HARDFOC_BOARD_VORTEX_V1) || !defined(HARDFOC_BOARD_SELECTED)
 #include "hf_functional_pin_config_vortex_v1.hpp"
 #else
-#error "No supported board variant defined! Please define HARDFOC_BOARD_VORTEX_V1 or another supported board."
+#error                                                                                             \
+    "No supported board variant defined! Please define HARDFOC_BOARD_VORTEX_V1 or another supported board."
 #endif
 
 #endif // HF_FUNCTIONAL_PIN_CONFIG_HPP
